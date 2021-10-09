2a27b1d6afc84ef8e42f41c91911bbaa1008019a
shaarangg <shaaranggsingh@gmail.com>
2021-10-10 02:14:19 +0530

    fix: Minor fix

---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+// func TestGetUserID(t *testing.T) {
+// 	res, err := http.Get("http://localhost:8081/users/6161f2e3e6cc8dbf2a559459")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	body, err := ioutil.ReadAll(res.Body)
+
+// }
